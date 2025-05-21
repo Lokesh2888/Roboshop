298645a05b1dc1e2e3ba05f51f4c9d2478048322
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-21 17:54:49 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="pothina.store"
 for instance in ${Instances[@]}
 do
    INSTANCE_ID= $(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0016b491173d15acd --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
-   if [$instance != frontend]
+   if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids i-0123456789abcdef0 --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)
     else
