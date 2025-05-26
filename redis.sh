d39ff9b6a65c49cc05ba1077c48afccd20b527fe
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-26 13:21:55 +0530

    hello world started

--- redis.sh
+++ redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-START_TIME=$(date %s)
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -42,3 +42,17 @@ VALIDATE $?"Enabling the redis 7"
 
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Instlling the redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "Started Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
